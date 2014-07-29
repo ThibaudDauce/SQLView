0ec9d4513b92e75ea43cf729bd9fbb1b69f667ba
Thibaud Dauce <thibaud@dauce.fr>
2014-07-29 15:06:06 +0200

    Add database connection

--- src/ThibaudDauce/SQLView/Builder.php
+++ src/ThibaudDauce/SQLView/Builder.php
@@ -1,5 +1,43 @@
 <?php namespace ThibaudDauce\SQLView;
 
+use Illuminate\Database\Query\Builder as QueryBuilder;
+
 class Builder {
 
+  public $connection;
+
+  /**
+   * Create a new database View manager.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->connection = \DB::connection();
+  }
+
+
+  /**
+   * Get the database connection.
+   *
+   * @return \Illuminate\Database\Connection
+   */
+  protected function getConnection()
+  {
+    return $this->connection;
+  }
+
+  /**
+   * Get a new query builder instance for the connection.
+   *
+   * @return \Illuminate\Database\Query\Builder
+   */
+  protected function newBaseQueryBuilder()
+  {
+    $connection = $this->getConnection();
+
+    $grammar = $connection->getQueryGrammar();
+
+    return new QueryBuilder($connection, $grammar, $connection->getPostProcessor());
+  }
 }
